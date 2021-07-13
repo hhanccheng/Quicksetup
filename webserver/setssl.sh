958acf3bb94cdbe3cd19ff644a7a9d4f1e05dc0b
hhanccheng <hccao12o@gmail.com>
2021-07-13 02:20:47 -0400

    71321220

--- webserver/setssl.sh
+++ webserver/setssl.sh
@@ -9,6 +9,6 @@ pacman -Sy certbot
 certbot certonly --webroot --email $email -d www.$domain -d $domain -w /usr/share/nginx/html
 mv qsslnginx.conf /etc/nginx/nginx.conf
 sed -i "s/example.com/$domain/g" /etc/nginx/nginx.conf
-systemctl start httpd mariadb nginx
+systemctl restart httpd mariadb nginx
 
 # auto renew
