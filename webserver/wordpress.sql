2af1530048048fbeb1116a36a2e60abba347037c
hhanccheng <hccao12o@gmail.com>
2021-07-13 16:22:24 -0400

    71321422

--- webserver/wordpress.sql
+++ webserver/wordpress.sql
@@ -1,5 +1,3 @@
-IF (EXISTS(SELECT * FROM master.dbo.sysdatabases WHERE dbid=db_ID('wordpress')))
-DROP DATABASE wordpress;
 CREATE DATABASE wordpress;
 CREATE USER wpadmin@localhost;
 SET PASSWORD FOR wpadmin@localhost= PASSWORD("wpasswd123");
